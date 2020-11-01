8fd4affec38d7166b157a1f1aa595bdfc5b93ec1
Nguyen Duong <nguyen1au@hotmail.com>
2020-11-01 16:33:33 +1000

    Setup stage frame and basic player movement

---
+++ Assets/Scripts/GroundCheck.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GroundCheck : MonoBehaviour
+{
+    GameObject player;
+
+    private void Start() {
+        player = gameObject.transform.parent.gameObject;
+    }
+    private void OnCollisionEnter2D(Collision2D other) {
+        if (other.collider.tag == "Terrain"){
+            player.GetComponent<Player>().isGrounded = true;
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D other) {
+        if (other.collider.tag == "Terrain"){
+            player.GetComponent<Player>().isGrounded = false;
+        }
+    }
+}
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -2,25 +2,56 @@
 using System.Collections.Generic;
 using UnityEngine;
 using Mirror;
+using System;
 
 public class Player : NetworkBehaviour
 {
+
+    public float moveSpeed = 5;
+    public float jumpForce = 6;
+    public bool isGrounded = false;
+    Rigidbody2D rb;
+
+    private void Awake() {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
     void HandleMovement(){
         if (isLocalPlayer){
             float moveHorizontal = Input.GetAxis("Horizontal");
-            float moveVertical = Input.GetAxis("Vertical");
-
-            Vector3 movement = new Vector3(0.1f * moveHorizontal, moveVertical * 0.1f, 0);
-
-            transform.position = transform.position + movement;
+            //Vector3 movement = new Vector3(0.1f * moveHorizontal, moveVertical * 0.1f, 0);
+            if (moveHorizontal > 0) {
+                //rb.AddForce(new Vector2(Math.Abs(moveHorizontal)*moveSpeed,0),ForceMode2D.Force);
+                rb.velocity = new Vector2(Math.Abs(moveHorizontal)*moveSpeed,rb.velocity.y);
+            } else if (moveHorizontal < 0){
+                //rb.AddForce(new Vector2(-Math.Abs(moveHorizontal)*moveSpeed,0),ForceMode2D.Force);
+                rb.velocity = new Vector2(Math.Abs(moveHorizontal)*-moveSpeed,rb.velocity.y);
+            } else {
+                //rb.velocity = new Vector2(0,rb.velocity.y);
+            }
+            //transform.position = transform.position + movement;
         }
     }
 
+    void Jump(){
+        isGrounded = false;
+        rb.AddForce(new Vector2(0,jumpForce), ForceMode2D.Impulse);
+    }
+
     private void Update() {
-        
+        if (Input.GetButtonDown("Jump") && isGrounded){
+            Jump();
+        }
+
+
+
     }
 
     private void FixedUpdate() {
         HandleMovement();
+        
     }
+    
+
+
 }
